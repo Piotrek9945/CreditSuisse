c627469b60a14c3667232dd39226bf07f782926c
Piotr Babiuch <pbabiuch@taxusit.com.pl>
2019-06-09 01:48:15 +0200

    test jdbc

--- src/main/java/com/test/creditsuisse/HelloGradle.java
+++ src/main/java/com/test/creditsuisse/HelloGradle.java
@@ -1,12 +1,26 @@
 package com.test.creditsuisse;
 
+import org.hsqldb.Statement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class HelloGradle {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Gradle!");
+    public static void main(String[] args) throws SQLException {
+        Connection c = DriverManager.getConnection("jdbc:hsqldb:file:c:\\Users\\Piotrek\\TaxusIt\\workspace_backend\\CreditSuisse\\db\\creditsuissedb", "admin", "password");
+        ResultSet aaa = c.createStatement().executeQuery("SELECT * FROM aaa");
+        while (aaa.next()) {
+            int id = aaa.getInt("id");
+            String name = aaa.getString("name");
+            System.out.println(id + " " + name);
+        }
     }
 
-    public int tescik() {
+    public int tescik() throws SQLException {
+
         return 3;
     }
 }
